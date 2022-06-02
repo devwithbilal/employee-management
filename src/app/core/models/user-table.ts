e62e99074b8ac02ba6d9b26ae6ceee4efbcbc7e5
devwithbilal <97337381+devwithbilal@users.noreply.github.com>
2022-06-02 17:03:18 +0500

    duwf

---
+++ src/app/core/models/user-table.ts
@@ -0,0 +1,10 @@
+export class UserTable {
+
+    id: number = 0;
+    userName: string = "";
+    userTask: string = "";
+    status: string = "";
+    created: Date = new Date();
+
+
+}
--- src/app/user-table/user-table.component.ts
+++ src/app/user-table/user-table.component.ts
@@ -2,8 +2,11 @@ import { SafeKeyedRead } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import {  FormBuilder, FormGroup, FormArray, Validators  } from '@angular/forms';
 import { EmployeeTask } from '../core/models/employee-task';
+import { UserTable } from '../core/models/user-table';
 
-interface userData extends EmployeeTask{}
+interface userData extends UserTable{
+  isNew: boolean,
+}
 
 
 @Component({
@@ -13,13 +16,75 @@ interface userData extends EmployeeTask{}
 })
 export class UserTableComponent implements OnInit {
 
+  userTables: userData [] = [];
+  created: Date = new Date();
+  identity: number = 0 ;
+  optionList: string[]= ['Assign','In-Progress','Hold','Completed'];
+  selected: string = "Assigning";
+  userTable: FormGroup  = this.fb.group({
+    tableRows: this.fb.array([])
+  });
+  touchedRows: any[] = [];
 
-
-  constructor(formbuilder:FormBuilder) { }
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    // this.setuserTable('Saad','Validation Form',this.created,'Assign');
+    // this.setuserTable('Bilal','Validation Form',this.created,'In-Progress');
+    // this.setuserTable('Jamil','Validation Form',this.created,'Hold');
+    // this.setuserTable('Furqan','Validation Form',this.created,'Assign');
+    
+    // this.userTable = this.fb.group({
+    //   tableRows: this.fb.array([])
+    // });
+    this.addRow();
+
   }
 
-  
+  addRow() {
+    const control =  this.userTable.get('tableRows') as FormArray;
+    control.push(this.setuserTable('','',this.created,'Assign'));
+  }
+
+  setuserTable(userName:string,userTask: string, created: Date,status:string,isNew: boolean = false){ 
+    // let userTable : userData = {
+
+    //   id: this.identity,
+    //   userName: userName,
+    //   userTask: userTask,
+    //   created: created,
+    //   status: status,
+    //   isNew: isNew,
+    // }
+    // this.userTables.push(userTable);
+    // this.identity++;
+    this.identity++;
+    return this.fb.group({
+      id: [this.identity],
+      userName: [userName, [Validators.required]],
+      userTask: [userTask],
+      created: [created],
+      status: [status],
+      isNew: [isNew],
+    });
+  }
+
+  newTask(usertable:userData){
+    this.setuserTable(usertable.userName, usertable.userTask, new Date(), usertable.status, true)
+  }
+
+  get getFormControls() {
+    const control = this.userTable.get('tableRows') as FormArray;
+    console.log("control",control)
+    return control;
+  }
+  submitForm(){
+    const control = this.userTable.get('tableRows') as FormArray;
+    this.touchedRows = control.controls.filter(row => row.touched).map(row => row.value);
+    console.log(this.touchedRows);
+  }
+
+
+
 
 }
